d5a9c502d10400a543012cbaed583d49640bfceb
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2022-08-30 10:50:39 -0500

    Init

---
+++ Source/Grip/Debug.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Debug.h"
+
+// Sets default values
+ADebug::ADebug()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADebug::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADebug::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
